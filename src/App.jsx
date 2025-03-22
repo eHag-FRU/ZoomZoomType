fb2b0ef0a9fb4ef6e8a6c94985c38c6bfc8b283b
GennieG2 <gurariyg@gmail.com>
2025-03-22 17:51:15 -0400

    Empty React Project w Home component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home.jsx';
+import NavBar from './components/NavBar.jsx';
+
+function App() {
+
+  return (
+    <div className='app-container'>
+      <NavBar></NavBar>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Home' element={<Home/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const NavBar = () => {
+
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default NavBar;
