21434e69430b56c0688d7a900d798e70f7321b25
Nick Neitenbach <nneiten1@kent.edu>
2025-03-24 08:44:54 -0400

    Create FaQ.jsx

---
+++ src/components/FaQ.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+const NavBar = () => {
+
+
+    return (
+        <div>
+            FaQ
+        </div>
+    )
+}
+
+export default NavBar;
