880960650a7dc756ce6ade066a9379e97cae0361
Nick Neitenbach <nneiten1@kent.edu>
2025-03-31 11:13:30 -0400

    About Page created
    empty

---
+++ client/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+
+const About = () => {
+
+    return (
+        <div>
+            About
+        </div>
+    )
+}
+
+export default About;
