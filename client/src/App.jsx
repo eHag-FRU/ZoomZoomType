caa299b1cbc1613854ec11365318cdda3efd9df9
GennieG2 <gurariyg@gmail.com>
2025-04-06 15:17:18 -0400

    imported pages to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from './components/Home.jsx';
 import NavBar from './components/NavBar.jsx';
 import FaQ from './components/FaQ.jsx';
+import About from './components/About.jsx';
+import ProfilePage from './components/ProfilePage.jsx';
+
 
 function App() {
 
