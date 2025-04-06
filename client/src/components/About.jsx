02250bca983336805b4e016704ae5de28b3417f8
Nick Neitenbach <nneiten1@kent.edu>
2025-04-06 19:12:03 -0400

    About Page
    Basic structure done, needs fine tuning and tweaks. Not fully responsive

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -7,36 +7,19 @@ const About = () => {
 
     return (
         <div style={{ backgroundColor: "#002F5C", minHeight: "100vh" }}>
-            <div className="container my-5">
-                <div className="container my-5">
-                    <h1 className="text-center text-white">About Us</h1>
-                    <p className="text-center text-white">ZoomZoomType</p>
+            <div className="">
+                <div className="container py-2">
+                    <h1 className="text-center" style={{ color: "#ffffff" }}>About Us</h1>
+                    <p className="text-center" style={{ color: "#FF9900" }}>ZoomZoomType</p>
                 </div>
-                <hr />
+                <hr style={{ border: "0.15rem solid black" }} />
 
 
-                <div className="text-white">
-                    <p>
-                        Text section regarding what we do as a group,
-                        how we started the idea, yadda yadda yadda <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum.
-                    </p>
-                </div>
-
-                <div className="text-white">
-                    <div className="about-image-placeholder">
-                        Picture detailing service offered
-                    </div>
-
-                    <div className="bottom-text-section">
-                        <p>
-                            Another text section regarding what we do as a group,
+                <div className="container text-white py-2">
+                    <div className="row">
+                        <div className="col-md-4 offset-md-9 p-2">
+                            <p>
+                            Text section regarding what we do as a group,
                             how we started the idea, yadda yadda yadda <br />
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -45,7 +28,28 @@ const About = () => {
                             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
                             deserunt mollit anim id est laborum.
-                        </p>
+                            </p>
+                        </div>
+                    </div>
+                    
+                </div>
+
+
+                <div className="container text-white py-2">
+                    <div className="row">
+                        <div className="col-md-4 offset-md-1 p-2">
+                            <p>
+                                Another text section regarding what we do as a group,
+                                how we started the idea, yadda yadda yadda <br />
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
