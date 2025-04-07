6f4a88cb99987ac676b9ebd54f5f3c7a7e940e14
Elliott Hager <ehager2@kent.edu>
2025-04-07 12:23:30 -0400

    Added some of the icons and having it scale things. Need to fix some of the coloring, but it should be okay

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -12,19 +12,19 @@ const NavBar = () => {
             <div className="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center gap-2">
 
                 <div className="d-flex flex-wrap align-items-center gap-2">
-                    <a className='nav-logo' href='/'><FontAwesomeIcon icon="fa-solid fa-car-on"/></a>
+                    <a className='nav-logo' href='/'><FontAwesomeIcon icon="fa-solid fa-car-on" style={{ fontSize: '1.5em' }}/></a>
                     <h5 className="mb-0 me-2">Zoom Zoom Type</h5>
-                    <a className='nav-anchor-visited nav-anchor-hover' href='/games'><FontAwesomeIcon icon="fa-solid fa-keyboard"/></a>
-                    <a className='nav-anchor-visited nav-anchor-hover' href='/leaderBoard'><FontAwesomeIcon icon="fas fa-crown"/></a>
-                    <a className='nav-anchor-visited nav-anchor-hover' href='/about'><FontAwesomeIcon icon="fas fa-info-circle"/></a>
-                    <span className="ms-2">Average WPM: <strong><p id="highestUserWPM">55</p></strong></span>
+                    <a className='nav-anchor-visited nav-anchor-hover' href='/games'><FontAwesomeIcon icon="fa-solid fa-keyboard" style={{ fontSize: '1.5em' }}/></a>
+                    <a className='nav-anchor-visited nav-anchor-hover' href='/leaderBoard'><FontAwesomeIcon icon="fas fa-crown" style={{ fontSize: '1.5em' }}/></a>
+                    <a className='nav-anchor-visited nav-anchor-hover' href='/about'><FontAwesomeIcon icon="fas fa-info-circle" style={{ fontSize: '1.5em' }}/></a>
+                    <span className="ms-2">Average WPM: <strong><p id="highestUserWPM"></p></strong></span>
                 </div>
 
                 {/* Gap is here */}
 
                 <div className="d-flex align-items-center gap-3 ms-md-3 mt-2 mt-md-0">
-                <i className="fas fa-cog"></i>
-                <a href='/login'><i className="fas fa-user"></i></a>
+                <a className="" href='/settings'><FontAwesomeIcon icon="fas fa-cog" style={{ fontSize: '1.5em' }}/></a>
+                <a className='' href='/login'><FontAwesomeIcon icon="fas fa-circle-user" style={{ fontSize: '1.5em' }}/></a>
                 </div>
 
             </div>
