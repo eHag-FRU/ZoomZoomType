7fb50f90f76750dd0467dc65b6a26367efc03a1c
SunCh1p <blaha.s.connor@gmail.com>
2025-04-08 22:28:43 -0400

    started gamepage, modified navbar to use react links, added nav-achor-hover:hover * and custom btn stuff to app.css

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 
 
 
 const NavBar = () => {
 
-
     return (
         <div className="theme-d5 text-white px-3 py-2">
             <div className="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center gap-2">
-
                 <div className="d-flex flex-wrap align-items-center gap-2">
-                    <a className='nav-logo' href='/'><FontAwesomeIcon icon="fa-solid fa-car-on" style={{ fontSize: '1.5em' }}/></a>
-                    <h5 className="mb-0 me-2">Zoom Zoom Type</h5>
-                    <a className='nav-anchor-visited nav-anchor-hover' href='/games'><FontAwesomeIcon icon="fa-solid fa-keyboard" style={{ fontSize: '1.5em' }}/></a>
-                    <a className='nav-anchor-visited nav-anchor-hover' href='/leaderBoard'><FontAwesomeIcon icon="fas fa-crown" style={{ fontSize: '1.5em' }}/></a>
-                    <a className='nav-anchor-visited nav-anchor-hover' href='/about'><FontAwesomeIcon icon="fas fa-info-circle" style={{ fontSize: '1.5em' }}/></a>
-                    <span className="ms-2">Average WPM: <strong><p id="highestUserWPM"></p></strong></span>
+                    <Link className='d-flex gap-2 me-2 nav-anchor-visited nav-achor-hover' to='/' style={{textDecoration:'none'}}>
+                        <FontAwesomeIcon icon='fa-solid fa-car-on' style={{ fontSize: '1.5em' }}/>
+                        <h5 className='mb-0'>Zoom Zoom Type</h5>
+                    </Link>
+                    <Link className='nav-anchor-visited nav-anchor-hover' to='/games'><FontAwesomeIcon icon="fa-solid fa-keyboard" style={{ fontSize: '1.5em' }}/></Link>
+                    <Link className='nav-anchor-visited nav-anchor-hover' to='/leaderBoard'><FontAwesomeIcon icon="fas fa-crown" style={{ fontSize: '1.5em' }}/></Link>
+                    <Link className='nav-anchor-visited nav-anchor-hover' to='/about'><FontAwesomeIcon icon="fas fa-info-circle" style={{ fontSize: '1.5em' }}/></Link>
+                    <span className="ms-2 ">Average WPM: <strong><p id="highestUserWPM"></p></strong></span>
                 </div>
 
                 {/* Gap is here */}
 
                 <div className="d-flex align-items-center gap-3 ms-md-3 mt-2 mt-md-0">
-                <a className="" href='/settings'><FontAwesomeIcon icon="fas fa-cog" style={{ fontSize: '1.5em' }}/></a>
-                <a className='' href='/login'><FontAwesomeIcon icon="fas fa-circle-user" style={{ fontSize: '1.5em' }}/></a>
+                    <Link className='nav-anchor-visited nav-anchor-hover' to='/settings'><FontAwesomeIcon icon="fas fa-cog" style={{ fontSize: '1.5em' }}/></Link>
+                    <Link className='nav-anchor-visited nav-anchor-hover' to='/login'><FontAwesomeIcon icon="fas fa-circle-user" style={{ fontSize: '1.5em' }}/></Link>
                 </div>
 
             </div>
