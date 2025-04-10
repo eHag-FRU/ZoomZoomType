4515fbf92151cb7ac203f98b8b8e22d8bd064231
SunCh1p <blaha.s.connor@gmail.com>
2025-04-10 01:12:23 -0400

    started work on typing game

--- client/src/components/GamePage.jsx
+++ client/src/components/GamePage.jsx
@@ -1,11 +1,19 @@
-import React, {useState} from 'react';
- 
+import React, {useState, useEffect, useRef} from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const GamePage = () => {
   const [mode, setMode] = useState("Solo Quotes");
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(60);
   const [wpm, setWpm] = useState(0);
-  const [text, setText] = useState("");
+  const [targetText, setTargetText] = useState("Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos");
+  const [words, setWords] = useState([]);
+  const [typedText, setTypedText] = useState("");
+
+  useEffect(() => {
+    const Words = targetText.split(' ').filter(word => word.trim() !== '');
+    setWords(Words);
+  }, [])
 
   //got this from chatgpt
   function formatTime(time){
@@ -14,8 +22,14 @@ const GamePage = () => {
     return `${String(minutes).padStart(2,'0')}:${String(seconds).padStart(2,'0')}`;
   }
 
+  const navigate = useNavigate();
+  function handleClick(page){
+      navigate(page);
+  }
+
 
   return (
+
     <div className="container-fluid d-flex flex-column flex-grow-1 align-items-center m-5">
       <div className="row w-75 rounded p-4 theme-l2 fw-bold mb-4">
         <div className="col-12 col-lg-4 text-center mt-2 mb-2 fs-5">Mode: {mode}</div>
@@ -23,16 +37,11 @@ const GamePage = () => {
         <div className="col-12 col-lg-4 text-center mt-2 mb-2 fs-5">wpm: {wpm}</div>
       </div>
       <div className="w-75 p-5 fs-5">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
-        </p>
+        {words}
       </div>
       <input className="w-50 m-6"/>
       <div className="container mt-5">
-        <button className="btn btn-lg custom-btn theme-l2 mb-3 fw-bold">Home</button>
+        <button onClick={() => handleClick('/Home')} className="btn btn-lg custom-btn theme-l2 mb-3 fw-bold">Home</button>
       </div>
     </div>
   )
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -41,7 +41,7 @@ const Home = (alreadyLoggedIn) => {
             </p>
             <div className="row w-100">
                 <div className="container-fluid d-flex justify-content-between gap-4 w-25">
-                    <button onClick={() => handleClick('GamePage')}className="btn btn-lg custom-accent-btn mb-3 fw-bold">Play Now</button>
+                    <button onClick={() => handleClick('/GamePage')}className="btn btn-lg custom-accent-btn mb-3 fw-bold">Play Now</button>
                     <button className="btn btn-lg custom-accent-btn mb-3 fw-bold">Modes</button>
                 </div>
             </div>
