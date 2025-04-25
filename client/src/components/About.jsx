76b344a35bb3f5534dcad0904104e781241ab07f
Nick Neitenbach <nneiten1@kent.edu>
2025-04-24 22:30:11 -0400

    about us

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reactLogo from '../assets/react.svg'
+import reactLogo from '../assets/zoomLogo.png'
 
 const About = () => {
 
@@ -15,42 +15,53 @@ const About = () => {
                 <p className="text-center" style={{ color: "#FF9900" }}>ZoomZoomType</p>
             </div>
             <hr style={{ border: "0.15em solid black" }} />
+            
 
             { /* About our group, how the idea started etc. */ }
-            <div className="container-fluid text-white py-5 me-5">
-                <div className="row justify-content-end me-5">
-                    <div className="col-12 col-md-10 col-lg-5 pe-lg-1">
-                        <p style={{ fontSize: "1.1em" }}>
-                        Text section regarding what we do as a group,
-                        how we started the idea, yadda yadda yadda <br />
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                        deserunt mollit anim id est laborum.
-                        </p>
+            <div className="container text-white">
+                <div className="row align-items-stretch">
+                    {/* Image on the left */}
+                    <div className="col-12 col-md-6 col-lg-5 d-flex align-items-start">
+                        <div className="w-100 d-flex">
+                            <img
+                                src={reactLogo}
+                                alt="Logo"
+                                className="img-fluid"
+                                style={{ borderRadius: '1rem', maxHeight: '500px', width: '100%', height: 'auto', objectFit: "contain" }}
+                            />
+                        </div>
+                    </div>
+
+                        
+                    {/* Text on the right — restored to original column with padding */}
+                    <div className="col-12 col-md-11 col-lg-7 pt-5 ps-lg-4">
+                        <div>
+                            <p style={{ fontSize: "1.1em" }}>
+                                Text section regarding what we do as a group,
+                                how we started the idea, yadda yadda yadda
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
+    
+                
 
 
             { /* Another text section? */ }
-            <div className="container-fluid text-white py-3 ms-5 ps-5">
-                <div className="row justify-content-start">
-                    <div className="col-12 col-md-6 col-lg-5 ps-lg-1">
-                        { /* Image above paragraph */}
-                        <img
-                            src={reactLogo}
-                            alt="Logo"
-                            className="img-fluid mb-3"
-                            style={{ borderRadius: '8px'}}
-                        />
-
+            <div className="container text-white py-1">
+                <div className="row justify-content-center">
+                    <div className="col-12 col-lg-11 mx-auto">
                         <p style={{ fontSize: "1.1em"}}>
                             Another text section regarding what we do as a group,
-                            how we started the idea, yadda yadda yadda <br />
+                            how we started the idea, yadda yadda yadda
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
